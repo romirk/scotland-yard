c66d47b46529347ed2e8b623b4af7f726bfc2ffb
Romir K <romir.kulshrestha@gmail.com>
2021-07-01 19:48:50 +0530

    basic properties

--- game/player.js
+++ game/player.js
@@ -1,8 +1,36 @@
-let Player = (name, color, location, isMrX) => {
-    this.name = name;
-    this.color = color;
-    this.isMrX = isMrX;
-    this.location = location;
+function Player(name, color, location, isMrX) {
+    let name = name;
+    let color = color;
+    let isMrX = isMrX;
+    let location = location;
+
+    let tickets = isMrX ? {
+        taxi: 4,
+        bus: 3,
+        underground: 3,
+        black: 5
+    } : {
+        taxi: 10,
+        bus: 8,
+        underground: 4
+    };
+
+    // getters
+    this.getName = () => name;
+    this.getColor = () => color;
+    this.isMrX = () => isMrX;
+    this.getLocation = () => location;
+    this.getTickets = (type) => {}
+
+    //setters
+    this.setLocation = (loc) => location = 1 <= loc && loc <= 200 ? loc : location;
+    this.use = (type) => {
+        switch (type) {
+            case 'bus':
+
+        }
+    }
+
 }
 
 module.exports = Player;
--- game/scotland_yard.js
+++ game/scotland_yard.js
@@ -1,11 +1,42 @@
 let Player = require('player');
 
-let ScotlandYard = () => {
+const SURFACE_MOVE = [3, 8, 13, 18, 24];
+const MOVE_LIMIT = 24;
+
+let ScotlandYard = (game_id) => {
+
 
     this.game_info = {
+        id: game_id,
         number_of_players: 0,
         running: true,
-        players: []
+        players: [],
+        available_locations: [34, 174, 132, 26, 198, 141, 94, 29, 53, 13, 112, 103, 155, 138, 117, 91, 197, 50],
+        moves: 0,
+        turn: 0
+    }
+
+    this.addPlayer = (name, color, isMrX) => {
+        let index = Math.floor(Math.random() * this.available_locations.length);
+        let loc = this.available_locations[index];
+        this.available_locations.splice(index, 1);
+
+        this.game_info.players.forEach(player => {
+            if (color == player.color) {
+                return false;
+            }
+            if (isMrX && player.isMrX) {
+                return false;
+            }
+        });
+
+        let newPlayer = new Player(name, color, loc, isMrX);
+
+        if (isMrX)
+            this.players.unshift(newPlayer);
+        else
+            this.players.push(newPlayer);
+        return true;
     }
 };
 

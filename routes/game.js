6351930ca64bde8c4532cee3f9bac7931305c90d
Romir K <romir.kulshrestha@gmail.com>
2021-07-01 17:37:30 +0530

    echo

--- routes/game.js
+++ routes/game.js
@@ -10,4 +10,10 @@ router.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
+router.get('/echo/:t', (req, res) => {
+    console.log("game route");
+    let text = req.params.t;
+    res.send(text);
+});
+
 module.exports = router

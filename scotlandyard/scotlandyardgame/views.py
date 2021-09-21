b58dd0a8771b2a4696e90a74644f837853d79283
romir kulshrestha <romir.kulshrestha@gmail.com>
2021-09-21 20:47:45 +0200

    static

--- scotlandyard/scotlandyardgame/views.py
+++ scotlandyard/scotlandyardgame/views.py
@@ -1,22 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
-
 
 
 def index(request):
     template = loader.get_template('scotlandyardgame/index.html')
     context = {
-        'latest_question_list': [],
+        'action': '/',
+        'game_id': 'game_id',
+        'isjoining': False
     }
-    return HttpResponse(template.render(context, request))
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    if request.method == "POST":    print(request.POST)
+    return render(request, 'scotlandyardgame/index.html', context=context)

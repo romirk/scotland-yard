952bdd546cdd5bf3a49613050d7e38fed4757d9d
Romir Kulshrestha <romir.kulshrestha@gmail.com>
2021-10-01 17:20:39 +0200

    bug fixes

--- scotlandyardgame/consumers.py
+++ scotlandyardgame/consumers.py
@@ -6,6 +6,26 @@ from .multiplayer import *
 mapPlayerToConn: dict[str, AsyncWebsocketConsumer] = dict()
 
 
+class LobbyRTConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.game_id = self.scope['url_route']['kwargs']['game_id']
+        game = getGameByID(self.game_id)
+
+        print(f"ws-connecting: {self.channel_name} {self.game_id}")
+
+        if game is not None:
+            print("accepted")
+            await self.channel_layer.group_add(self.game_id, self.channel_name)
+            await self.accept()
+            await self.send("hello client")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.game_id, self.channel_name)
+
+    async def receive(self, text_data):
+        print(f"[ws/client] {text_data}")
+
+        
 class GameRTConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.game_id = self.scope['url_route']['kwargs']['game_id']
--- scotlandyardgame/routing.py
+++ scotlandyardgame/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 
-from .consumers import GameRTConsumer
+from .consumers import GameRTConsumer, LobbyRTConsumer
 
 websocket_urlpatterns = [
-    re_path( r'^ws/(?P<game_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$', GameRTConsumer.as_asgi())
+    re_path( r'^ws/lobby/(?P<game_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$', LobbyRTConsumer.as_asgi()),
+    re_path( r'^ws/game/(?P<game_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$', GameRTConsumer.as_asgi())
 ]

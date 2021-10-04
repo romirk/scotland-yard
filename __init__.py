e64da6eea1f778b70a634e3963988b09633eac99
Romir Kulshrestha <romir.kulshrestha@gmail.com>
2021-10-04 13:46:07 +0200

    port

--- __init__.py
+++ __init__.py
@@ -8,9 +8,10 @@ from django.core.management import call_command
 from scotlandyard import settings as sysettings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scotlandyard.settings')
+port = int(os.environ.get("PORT", 5000))
 
 django.setup()
 
 call_command("makemigrations", interactive=False)
 call_command("migrate", interactive=False)
-call_command("runserver")
+call_command("runserver", f"0.0.0.0:{port}")

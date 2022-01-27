996e3a2aac5d643c9beeedaaae6d9cde88f92aaa
romir kulshrestha <r.kulshrestha@student.tudelft.nl>
2022-01-27 22:15:02 +0530

    failed engine tests

--- scotlandyardgame/test_engine.py
+++ scotlandyardgame/test_engine.py
@@ -1,22 +1,21 @@
-from cgitb import text
 from uuid import uuid4
+
+from asgiref.sync import async_to_sync
+from channels.testing import WebsocketCommunicator
 from django.test import TestCase
 
+from .multiplayer import (createRoom, getGameByID, joinRoom,
+                          startRollCall)
 from .ws.GameConsumer import GameConsumer
 
-from .engine.main import ScotlandYard
-from .engine.constants import AVAILABLE_START_LOCATIONS
-from channels.testing import WebsocketCommunicator
-
-from .multiplayer import createRoom, getGameByID, getGameInfo, joinRoom, startRollCall
-
+### TODO uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 
 class EngineTestCase(TestCase):
     """
     Test game engine.
     """
 
-    async def setUp(self) -> None:
+    def setUp(self) -> None:
         self.test_game_id = createRoom()
         self.game = getGameByID(self.test_game_id)
 
@@ -30,12 +29,24 @@ class EngineTestCase(TestCase):
         self.communicators = [WebsocketCommunicator(
             GameConsumer.as_asgi(), f"/game/{self.test_game_id}") for _ in range(6)]
 
+        print("\033[32mset up succesfully\033[0m")
+
+    async def start_comms(self):
         for communicator in self.communicators:
             connected, subprotocol = await communicator.connect()
-            assert connected
+            self.assertTrue(connected)
+        print("connected")
 
         for i, player_id in enumerate(self.player_ids):
             await self.communicators[i].send_to(text_data=f"JOIN {player_id}")
             response = await communicator.receive_from()
+
+    async def do_roll_call(self):
+        await self.start_comms()
+        # do stuff
             
-        print("set up succesfully")
+
+    async def test_basic(self):
+        # await self.start_comms()
+        pass
+

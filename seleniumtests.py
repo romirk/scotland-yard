55b91199a52afc5846a60b1d99a5a5049df4a0f0
romir kulshrestha <r.kulshrestha@student.tudelft.nl>
2022-02-16 12:26:39 +0100

    updated tests

--- seleniumtests.py
+++ seleniumtests.py
@@ -1,11 +1,10 @@
+from concurrent.futures import thread
 import sys
 from threading import Thread
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-GAME_ID = None
 
 
 def run_detectives(game_id: str, number: int):
@@ -19,34 +18,25 @@ def run_detectives(game_id: str, number: int):
     driver.close()
 
 
-def run_mr_x():
-    global GAME_ID
-    driver = webdriver.Chrome(sys.argv[1])
-    driver.get(f"http://localhost:8000/")
-    assert "Scotland Yard" in driver.title
-    elem = driver.find_element_by_id("player_name")
-    elem.send_keys("x")
-    elem.send_keys(Keys.RETURN)
-    sleep(1.1)
-    GAME_ID = driver.execute_script("return GAME_ID")
-    while s := driver.execute_script(
-        "return document.getElementById('start').style.display !== 'initial';"
-    ):
-        sleep(0.5)
-    elem = driver.find_element_by_id("start")
-    elem.click()
-    sleep(10000)
-    driver.close()
+driver = webdriver.Chrome(sys.argv[1])
+driver.get(f"http://localhost:8000/")
+assert "Scotland Yard" in driver.title
+elem = driver.find_element_by_id("player_name")
+elem.send_keys("x")
+elem.send_keys(Keys.RETURN)
+sleep(1.1)
+GAME_ID = driver.execute_script("return GAME_ID")
 
+for i in range(1, 6):
+    t = Thread(target=run_detectives, args=(GAME_ID, i),daemon=True)
+    t.start() 
 
-mr_x_thread = Thread(target=run_mr_x)
-mr_x_thread.start()
-# mr_x_thread.join()
-sleep(10)
-print(GAME_ID)
-detective_threads = [
-    Thread(target=run_detectives, args=(GAME_ID, i)) for i in range(1, 6)
-]
-for thread in detective_threads:
-    thread.start()
-    # thread.join()
+while driver.execute_script(
+    "return document.getElementById('start').style.display !== 'initial';"
+):
+    sleep(0.5)
+elem = driver.find_element_by_id("start")
+elem.click()
+while True:
+    sleep(1)
+driver.close()
